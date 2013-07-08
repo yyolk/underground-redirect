52b5420406d02717d16f30cca2c6987c3a2cbfbe
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-07-08 18:44:34 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+tumblr-redirect
+===============
+
+simple expressjs app get around ownership of urls
+
+[try it](http://tumblr.yolk.cc)
+
+
+intended for [this app](https://github.com/yyolk/Chrome-Tumblr-Shortcut)  ([Chrome web store link](https://chrome.google.com/webstore/detail/tumblr/giikciflnljhpomkphelpoiheghamifc))
---
+++ app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+
+
+var keenIO = require('keen.io');
+
+// Configure instance. Only projectId and writeKey are required to send data.
+var keen = keenIO.configure({
+    projectId: process.env['KEEN_PROJECT_ID'],
+    writeKey: process.env['KEEN_WRITE_KEY']
+});
+
+
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('*', function(req, res){
+  res.redirect('https://drive.google.com/#folders/0B8IN6XOKkwRTbjlNZnJnM3BMRVE');
+  keen.addEvent("applaunch", {"count": 1}, function(err, res) {
+    if(err){
+      consoloe.log("keen error");
+    } else {
+      console.log("keen success");
+    }
+});
+});
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
