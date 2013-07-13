293d8fda9e8eda04183f7c983dd78794a183254b
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-07-13 16:21:28 -0500

    update link

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('*', function(req, res){
-  res.redirect('https://drive.google.com/folderview?id=0B8IN6XOKkwRTRktGR21vTHdfRXM&usp=sharing');
+  res.redirect('https://drive.google.com/folderview?id=0B8IN6XOKkwRTeExPOGQzODZvZVU&usp=sharing');
   keen.addEvent("applaunch", {"count": 1}, function(err, res) {
     if(err){
       consoloe.log("keen error");
